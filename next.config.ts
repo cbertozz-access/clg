5d3ff5a8f8553d5aac597d7af871ca9c4c7d1b7c
cbertozz-access <176544857+cbertozz-access@users.noreply.github.com>
2026-01-29 08:41:15 +1100

    Add S3 (amazonaws.com) to CSP connect-src for Marker.io uploads

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
             value: [
               "frame-ancestors 'self' https://*.builder.io https://builder.io",
               "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.googletagmanager.com https://*.google-analytics.com https://cdn.builder.io https://*.builder.io https://edge.marker.io https://*.marker.io",
-              "connect-src 'self' https://*.googletagmanager.com https://*.google-analytics.com https://*.analytics.google.com https://*.builder.io https://*.cloudfunctions.net https://sgtm.accesshire.net https://composable-lg.ts.r.appspot.com https://*.netlify.app https://*.marker.io https://api.marker.io",
+              "connect-src 'self' https://*.googletagmanager.com https://*.google-analytics.com https://*.analytics.google.com https://*.builder.io https://*.cloudfunctions.net https://sgtm.accesshire.net https://composable-lg.ts.r.appspot.com https://*.netlify.app https://*.marker.io https://api.marker.io https://*.amazonaws.com",
               "img-src 'self' data: https: blob:",
             ].join("; "),
           },
